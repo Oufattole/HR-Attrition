3fd22f19efe9fd2eabd13dd421d0b2db304677cf
Lion-Mod <70057706+Lion-Mod@users.noreply.github.com>
2021-01-03 18:48:48 +0000

    Update app.py

--- app.py
+++ app.py
@@ -15,6 +15,9 @@ def predict(model, input_df):
 
 def run():
 
+    # Expand the layout
+    st.set_page_config(layout = "wide")
+
     # Select whether or not to do single row or multiple row predictions
     add_selectbox = st.sidebar.selectbox("Single or Multi predict?", ("Single", "Multi"))
     
@@ -22,41 +25,51 @@ def run():
     st.title("HR Attrition")
     st.subheader("Using this app you can identify whether an employee(s) will potentially leave in the next six months and also why.")
 
+    # Initialise columns
+    col1, col2, col3, col4 = st.beta_columns(4)
+
     # Create individual app elements to provide input for single row prediction
     if add_selectbox == "Single":
 
-        Age = st.number_input("Age", min_value = 18, max_value = 100, step = 1)
-        BusinessTravel = st.select_slider("BusinessTravel", options = ['Non-Travel', 'Travel_Rarely', 'Travel_Frequently'])
-        DailyRate = st.number_input("DailyRate", min_value = 100, max_value = 1500, step = 1)
-        Department = st.select_slider("Department", options = ['Sales', 'Research & Development', 'Human Resources'])
-        DistanceFromHome = st.number_input("DistanceFromHome", min_value = 1, max_value = 30, step = 1)
-        Education = st.select_slider("Education", options = [1, 2, 3, 4, 5])
-        EducationField = st.select_slider("EducationField", options = ['Life Sciences', 'Other', 'Medical', 'Marketing', 'Technical Degree', 'Human Resources'])
-        EnvironmentSatisfaction = st.select_slider("EnvironmentSatisfaction", options = [1, 2, 3, 4])
-        Gender = st.select_slider("Gender", options = ["Female", "Male"])
-        HourlyRate = st.number_input("HourlyRate", min_value = 1, max_value = 100, step = 1)
-        JobInvolvement = st.select_slider("JobInvolvement", options = [1, 2, 3, 4])
-        JobLevel = st.select_slider("JobLevel", options = [1, 2, 3, 4, 5])
-        JobRole = st.select_slider("JobRole", options = ['Sales Executive', 'Research Scientist', 'Laboratory Technician', 'Manufacturing Director', 'Healthcare Representative', 'Manager', 'Sales Representative', 'Research Director', 'Human Resources'])
-        JobSatisfaction = st.select_slider("JobSatisfaction", options = [1, 2, 3, 4])
-        MaritalStatus = st.select_slider("MaritalStatus", options = ['Single', 'Married', 'Divorced'])
-        MonthlyIncome = st.number_input("MonthlyIncome", min_value = 1000, max_value = 20000, step = 1)
-        MonthlyRate = st.number_input("MonthlyRate", min_value = 2000, max_value = 27000, step = 1)
-        NumCompaniesWorked = st.slider("NumCompaniesWorked", min_value = 0, max_value = 9, step = 1)
-        Over18 = st.select_slider("Over18", options = ["N", "Y"])
-        OverTime = st.select_slider("OverTime", options = ["N", "Y"])
-        PercentSalaryHike = st.number_input("PercentSalaryHike", min_value = 10, max_value = 25, step = 1)
-        PerformanceRating = st.number_input("PerformanceRating", min_value = 3, max_value = 4, step = 1)
-        RelationshipSatisfaction = st.select_slider("RelationshipSatisfaction", options = [1, 2, 3, 4])
-        # StandardHours =
-        StockOptionLevel = st.select_slider("StockOptionLevel", options = [0, 1, 2, 3])
-        TotalWorkingYears = st.slider("TotalWorkingYears", min_value = 0, max_value = 40, step = 1)
-        TrainingTimesLastYear = st.slider("TrainingTimesLastYear", min_value = 0, max_value = 6, step = 1)
-        WorkLifeBalance = st.slider("WorkLifeBalance", min_value = 0, max_value = 4, step = 1)
-        YearsAtCompany = st.slider("YearsAtCompany", min_value = 0, max_value = 40, step = 1)
-        YearsInCurrentRole = st.slider("YearsInCurrentRole", min_value = 0, max_value = 18, step = 1)
-        YearsSinceLastPromotion = st.slider("YearsSinceLastPromotion", min_value = 0, max_value = 15, step = 1)
-        YearsWithCurrManager = st.slider("YearsWithCurrManager", min_value = 0, max_value = 18, step = 1)
+        with col1:
+            Age = st.number_input("Age", min_value = 18, max_value = 100, step = 1)
+            BusinessTravel = st.select_slider("BusinessTravel", options = ['Non-Travel', 'Travel_Rarely', 'Travel_Frequently'])
+            DailyRate = st.number_input("DailyRate", min_value = 100, max_value = 1500, step = 1)
+            Department = st.select_slider("Department", options = ['Sales', 'Research & Development', 'Human Resources'])
+            DistanceFromHome = st.number_input("DistanceFromHome", min_value = 1, max_value = 30, step = 1)
+            Education = st.select_slider("Education", options = [1, 2, 3, 4, 5])
+            EducationField = st.select_slider("EducationField", options = ['Life Sciences', 'Other', 'Medical', 'Marketing', 'Technical Degree', 'Human Resources'])
+            EnvironmentSatisfaction = st.select_slider("EnvironmentSatisfaction", options = [1, 2, 3, 4])
+            
+        with col2:
+            Gender = st.select_slider("Gender", options = ["Female", "Male"])
+            HourlyRate = st.number_input("HourlyRate", min_value = 1, max_value = 100, step = 1)
+            JobInvolvement = st.select_slider("JobInvolvement", options = [1, 2, 3, 4])
+            JobLevel = st.select_slider("JobLevel", options = [1, 2, 3, 4, 5])
+            JobRole = st.select_slider("JobRole", options = ['Sales Executive', 'Research Scientist', 'Laboratory Technician', 'Manufacturing Director', 'Healthcare Representative', 'Manager', 'Sales Representative', 'Research Director', 'Human Resources'])
+            JobSatisfaction = st.select_slider("JobSatisfaction", options = [1, 2, 3, 4])
+            MaritalStatus = st.select_slider("MaritalStatus", options = ['Single', 'Married', 'Divorced'])
+            MonthlyIncome = st.number_input("MonthlyIncome", min_value = 1000, max_value = 20000, step = 1)
+            
+        with col3:
+            MonthlyRate = st.number_input("MonthlyRate", min_value = 2000, max_value = 27000, step = 1)
+            NumCompaniesWorked = st.slider("NumCompaniesWorked", min_value = 0, max_value = 9, step = 1)
+            Over18 = st.select_slider("Over18", options = ["N", "Y"])
+            OverTime = st.select_slider("OverTime", options = ["N", "Y"])
+            PercentSalaryHike = st.number_input("PercentSalaryHike", min_value = 10, max_value = 25, step = 1)
+            PerformanceRating = st.number_input("PerformanceRating", min_value = 3, max_value = 4, step = 1)
+            RelationshipSatisfaction = st.select_slider("RelationshipSatisfaction", options = [1, 2, 3, 4])
+            # StandardHours =
+            StockOptionLevel = st.select_slider("StockOptionLevel", options = [0, 1, 2, 3])
+            
+        with col4:
+            TotalWorkingYears = st.slider("TotalWorkingYears", min_value = 0, max_value = 40, step = 1)
+            TrainingTimesLastYear = st.slider("TrainingTimesLastYear", min_value = 0, max_value = 6, step = 1)
+            WorkLifeBalance = st.slider("WorkLifeBalance", min_value = 0, max_value = 4, step = 1)
+            YearsAtCompany = st.slider("YearsAtCompany", min_value = 0, max_value = 40, step = 1)
+            YearsInCurrentRole = st.slider("YearsInCurrentRole", min_value = 0, max_value = 18, step = 1)
+            YearsSinceLastPromotion = st.slider("YearsSinceLastPromotion", min_value = 0, max_value = 15, step = 1)
+            YearsWithCurrManager = st.slider("YearsWithCurrManager", min_value = 0, max_value = 18, step = 1)
 
         output = ""
 
@@ -101,7 +114,7 @@ def run():
             output = '$' + str(output)
 
         # Output of the prediction
-        st.success('{}'.format(output), 'this person will leave.')
+        st.success('{} this person will leave.'.format(output))
 
         # NEED REASON PLOT HERE
 
