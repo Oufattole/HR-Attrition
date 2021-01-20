bd6d9361475dfc4983780e52551bfbd391ca087d
Lion-Mod <70057706+Lion-Mod@users.noreply.github.com>
2021-01-20 08:49:32 +0000

    Updated function documentation.

--- app.py
+++ app.py
@@ -5,46 +5,88 @@ import pandas as pd
 import numpy as np
 import joblib
 import shap
-import lime
-from lime import lime_tabular
-
 
 # IMPORT/LOAD
-gbc = load_model('models/gbc_model') # GBC model
-final_gbc = load_model('models/final_gbc') # final GBC model
+## FINALIZED MODEL
+final_gbc = load_model('models/final_gbc') 
 
-prep_pipe = joblib.load("prep_pipe.pkl") # Pycaret preparation pipeline
+## PYCARET PIPELINE
+prep_pipe = joblib.load("prep_pipe.pkl")
 
-train_data = pd.read_csv("data/preprocessed_data.csv") # Preprocessed data used in training
+# DATA
+train_data = pd.read_csv("data/preprocessed_data.csv") 
 original = pd.read_csv("data/HR Employee Attrition.csv")
 
 
+# FUNCTIONS
 def get_prediction_df(input_df, model):
     """
-    Get prediction using the gbc model
+    Take the original dataframe and add the prediction label (Yes/No) and the score the model thinks for this label
+
+    Args:
+        input_df (pandas.DataFrame): dataframe from the user inputted data
+        model : model from pycaret
+
+    Returns:
+        pandas.DataFrame: original dataframe w/ prediction + score
     """
     predictions_df = predict_model(estimator = model, data = input_df)
     return predictions_df
 
 def process_using_pipeline(input_df, pipe):
     """
-    Process the data using the Pycaret pipeline
+    Process the user inputted dataframe using the pycaret processing pipeline
+
+    Args:
+        input_df (pandas.DataFrame): dataframe from the user inputted data
+        pipe (pycaret pipeline): transformation pipeline from pycaret
+
+    Returns:
+        pandas.DataFrame: processed pandas.DataFrame
     """
     return pipe.transform(input_df)
 
-def st_explanation(plot, height=None):
+def handle_one_hot_encoding(processed_data, shap_values, ohe_features):
     """
-    Plot the explanation within the streamlit app
+    Takes list of `ohe_features` and combines them back into one feature for the prediction plot.
+
+    Args:
+        processed_data (pandas.DataFrame): the input dataframe processed using the processing steps of the model
+        shap_values (): shap values of the processed_data
+        ohe_features (list): list of features that were one hot encoded
+
+    Returns:
+        pandas.DataFrame: dataframe with columns feature | shap_value
+    """
+    summary_df = pd.DataFrame([processed_data.columns, shap_values]).T
+    summary_df.columns = ['feature', 'shap_value']
+
+    mapping = {}
+
+    for feature in summary_df.feature.values:
+        mapping[feature] = feature
+        for prefix, alternative in zip(ohe_features, ohe_features):
+            if feature.startswith(prefix):
+                mapping[feature] = alternative
+                break
+
+    summary_df['feature'] = summary_df.feature.map(mapping)
+
+    shap_df = summary_df.groupby('feature').sum().reset_index()
+
+    return shap_df
+
+def st_explanation(plot, height):
+    """
+    Plot the shap plot for a single explanation
+
+    Args:
+        plot (shap.force_plot): shap.force_plot input
+        height (int): Height of the html component displayed.
     """
     html = f"<head>{shap.getjs()}</head><body>{plot.html()}</body>"
     components.html(html, height = height)
 
-def st_shap(plot, height=None):
-    """
-    Plot the explanation within the streamlit app
-    """
-    html = f"<head>{shap.getjs()}</head><body>{plot}</body>"
-    components.html(html, height = height)
 
 def run():
 
@@ -53,7 +95,7 @@ def run():
 
     # Select whether or not to do single row or multiple row predictions
     add_selectbox = st.sidebar.selectbox("Single or Multi predict?", ("Single", "Multi"))
-    
+
     # Title
     st.title("HR Attrition")
     st.subheader("Using this app you can identify whether an employee(s) will potentially leave in the next six months and also why.")
@@ -73,7 +115,7 @@ def run():
             Education = st.select_slider("Education", options = [1, 2, 3, 4, 5])
             EducationField = st.select_slider("EducationField", options = ['Life Sciences', 'Other', 'Medical', 'Marketing', 'Technical Degree', 'Human Resources'])
             EnvironmentSatisfaction = st.select_slider("EnvironmentSatisfaction", options = [1, 2, 3, 4])
-            
+
         with col2:
             Gender = st.select_slider("Gender", options = ["Female", "Male"])
             HourlyRate = st.number_input("HourlyRate", min_value = 1, max_value = 100, step = 1)
@@ -83,19 +125,17 @@ def run():
             JobSatisfaction = st.select_slider("JobSatisfaction", options = [1, 2, 3, 4])
             MaritalStatus = st.select_slider("MaritalStatus", options = ['Single', 'Married', 'Divorced'])
             MonthlyIncome = st.number_input("MonthlyIncome", min_value = 1000, max_value = 20000, step = 1)
-            
+
         with col3:
             MonthlyRate = st.number_input("MonthlyRate", min_value = 2000, max_value = 27000, step = 1)
             NumCompaniesWorked = st.slider("NumCompaniesWorked", min_value = 0, max_value = 9, step = 1)
-            #Over18 = st.select_slider("Over18", options = ["N", "Y"])
             OverTime = st.select_slider("OverTime", options = ["N", "Y"])
             PercentSalaryHike = st.number_input("PercentSalaryHike", min_value = 10, max_value = 25, step = 1)
             PerformanceRating = st.number_input("PerformanceRating", min_value = 3, max_value = 4, step = 1)
             RelationshipSatisfaction = st.select_slider("RelationshipSatisfaction", options = [1, 2, 3, 4])
-            # StandardHours =
             StockOptionLevel = st.select_slider("StockOptionLevel", options = [0, 1, 2, 3])
             TotalWorkingYears = st.slider("TotalWorkingYears", min_value = 0, max_value = 40, step = 1)
-            
+
         with col4:
             TrainingTimesLastYear = st.slider("TrainingTimesLastYear", min_value = 0, max_value = 6, step = 1)
             WorkLifeBalance = st.slider("WorkLifeBalance", min_value = 0, max_value = 4, step = 1)
@@ -106,7 +146,7 @@ def run():
 
         output = ""
 
-        input_dict = {"Age" : Age, 
+        input_dict = {"Age" : Age,
                     "BusinessTravel" :  BusinessTravel ,
                     "DailyRate" :  DailyRate ,
                     "Department" :  Department ,
@@ -140,7 +180,7 @@ def run():
 
         # Create row of data based upon the element inputs
         input_df = pd.DataFrame([input_dict])
- 
+
         # Make single prediction for data input using the model
         if st.button("Predict"):
 
@@ -157,35 +197,24 @@ def run():
             new_processed = process_using_pipeline(prediction_df, prep_pipe)
 
             # Create shap tree explainer
-            test = shap.TreeExplainer(final_gbc['trained_model'], data = train_data, model_output = "probability")
+            exp = shap.TreeExplainer(final_gbc['trained_model'], data = shap.sample(train_data, 1000), model_output = "probability")
 
             # Get shap values for new data
-            sv = test.shap_values(new_processed.iloc[0])
+            shap_values = exp.shap_values(new_processed.iloc[0], check_additivity = True)
 
-            
-            summary_df = pd.DataFrame([train_data.columns, sv]).T
-            summary_df.columns = ['feature', 'shap_value']
-
-            mapping = {}
-
-            for feature in summary_df.feature.values:
-                mapping[feature] = feature
-                for prefix, alternative in zip(['BusinessTravel', 'Department', 'EducationField', 'Gender', 'JobLevel', 'JobRole', 'MaritalStatus', 'OverTime'],
-                                            ['BusinessTravel', 'Department', 'EducationField', 'Gender', 'JobLevel', 'JobRole', 'MaritalStatus', 'OverTime']):
-                    if feature.startswith(prefix):
-                        mapping[feature] = alternative
-                        break
-
-            summary_df['feature'] = summary_df.feature.map(mapping)
-
-            shap_df = summary_df.groupby('feature').sum().reset_index()
+            # Handle the OHE columns and return the new shap values ready to plot
+            shap_df = handle_one_hot_encoding(processed_data=train_data,
+                                              shap_values=shap_values,
+                                              ohe_features = ['BusinessTravel', 'Department', 'EducationField', 'Gender', 'JobLevel', 'JobRole', 'MaritalStatus', 'OverTime'])
 
             # Plot the inputted data's shap plot
-            st_explanation(shap.force_plot(base_value = test.expected_value,
-                                            shap_values = shap_df.shap_value.values,
-                                            features = input_df,
-                                            feature_names = list(original.drop(["EmployeeNumber", "StandardHours", "EmployeeCount", "Over18", "Attrition"], axis = 1).columns),
-                                            text_rotation = 180))
+            st_explanation(shap.force_plot(base_value = exp.expected_value,
+                                           shap_values = shap_df.shap_value.values,
+                                           features = input_df,
+                                           feature_names = list(original.drop(["EmployeeNumber", "StandardHours", "EmployeeCount", "Over18", "Attrition"], axis = 1).columns),
+                                           text_rotation = 180,
+                                           out_names = prediction_label),
+                            height = 200)
 
 
     # Batch prediction (multiple people to predict)
